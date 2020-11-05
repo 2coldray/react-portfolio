f46e1f1aca778df51a63db7d166faf90e66fa68e
Ray Bray <rbray.bray2@gmail.com>
2020-11-05 00:57:13 -0500

    added basic layout to card for my portfolio page

--- src/containers/Portfolio/Portfolio.jsx
+++ src/containers/Portfolio/Portfolio.jsx
@@ -4,9 +4,21 @@ const Portfolio = () => {
     return (
         <div className="container mt-3 mb-2">
             <div className="row">
-                <div className="col-sm-6 bg-light mx-auto" id="image">
+                <div className="col-sm-6 bg-light mx-auto">
                     <h1 className="text-center">Portfolio</h1>
                     <hr />
+                    <div className="card text-center">
+                        <div className="card-header text-center">
+                            Weather Dashboard Application
+                        </div>
+                        <div className="card-body">
+                            <p>Screenshot</p>
+                        </div>
+                        <div className="card-footer sticky text-center">
+                            <p>Github Link</p>
+                            <p>Deployed Application Link</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
