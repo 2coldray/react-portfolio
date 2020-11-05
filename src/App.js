b147299bbc69b49eca77964b1082b35dbb25c16f
Ray Bray <rbray.bray2@gmail.com>
2020-11-04 21:45:37 -0500

    added and tested components and containers on the home page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-
+import Home from "./containers/Home/Home";
+import Portfolio from "./containers/Portfolio/Portfolio";
+import Contact from "./containers/Contact/Contact";
 
 function App() {
   return (
     <>
-    <Router>
-      <Navbar />
-      <Footer />
-    </Router>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+        <Footer />
+      </Router>
     </>
   );
 }
