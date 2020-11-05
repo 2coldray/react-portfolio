6289b83b896ae4f675d119164a21638ad8ff5112
Ray Bray <rbray.bray2@gmail.com>
2020-11-05 00:47:11 -0500

    added basic portfolio layout for portfolio page

--- src/containers/Portfolio/Portfolio.jsx
+++ src/containers/Portfolio/Portfolio.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Portfolio = () => {
     return (
-        <div>
-            <p>Portfolio Cards</p>
+        <div className="container mt-3 mb-2">
+            <div className="row">
+                <div className="col-sm-6 bg-light mx-auto" id="image">
+                    <h1 className="text-center">Portfolio</h1>
+                    <hr />
+                </div>
+            </div>
         </div>
     );
 };
